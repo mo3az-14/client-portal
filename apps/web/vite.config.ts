4f263acc80ddbed9b6b9aab8822edbce120e0b22
moaaz tarek <moaaz2tarik1@gmail.com>
2025-05-16 22:50:51 +0300

    removed resolve in tsconfig paths

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,4 +1,3 @@
-import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -10,12 +9,6 @@ export default ({ mode }: { mode: string }) => {
     const env = loadEnv(mode, process.cwd(), "VITE_")
     return defineConfig({
         plugins: [react(), tsconfigPaths(), tailwindcss(), nodePolyfills()],
-
-        resolve: {
-            alias: {
-                "@": path.resolve(__dirname, "./src"),
-            },
-        },
         server: {
             port: 5173,
             proxy: {
