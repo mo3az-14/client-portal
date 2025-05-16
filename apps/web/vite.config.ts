2bd8f5d29f62046086a09bc2506996214da0c4b5
moaaz tarek <moaaz2tarik1@gmail.com>
2025-05-16 22:35:49 +0300

    added tsconfigPaths to viteconfig.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,13 +3,13 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-
+import tsconfigPaths from "vite-tsconfig-paths"
 // https://vite.dev/config/
 export default ({ mode }: { mode: string }) => {
 
     const env = loadEnv(mode, process.cwd(), "VITE_")
     return defineConfig({
-        plugins: [react(), tailwindcss(), nodePolyfills()],
+        plugins: [react(), tsconfigPaths(), tailwindcss(), nodePolyfills()],
 
         resolve: {
             alias: {
