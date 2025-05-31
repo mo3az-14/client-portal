d285f5305262619581a4d98cf5568cf7ee607141
moaaz tarek <moaaz2tarik1@gmail.com>
2025-05-31 03:22:41 +0300

    cors

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -21,12 +21,12 @@ const PORT = process.env.SERVER_PORT;
 const app = express();
 
 console.log(process.env.CLIENT_URL)
-// app.use(cors({
-//     origin: process.env.CLIENT_URL,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//     credentials: true,
-// }));
-// app.options('*ss', cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+}));
+app.options('*ss', cors());
 // app.use('*ssc', logRequest)
 app.all("/api/auth/*s", toNodeHandler(auth));
 
